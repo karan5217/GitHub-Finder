3fb2445362683da2fba066c6366bf2ef1d9d4fa1
karan5217 <karan5217@gmail.com>
2020-07-25 22:41:54 +0530

    Updates

---
+++ precache-manifest.a5923c590709aa153929bf14400314ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb48b26389e2ba49cfb6093db3ef938",
+    "url": "/Github-Finder/index.html"
+  },
+  {
+    "revision": "db1529f812ffece116ec",
+    "url": "/Github-Finder/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "aeb509eedbdabf4a9b26",
+    "url": "/Github-Finder/static/js/2.1ef90c43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github-Finder/static/js/2.1ef90c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1529f812ffece116ec",
+    "url": "/Github-Finder/static/js/main.8e7cd874.chunk.js"
+  },
+  {
+    "revision": "1ea4e396fc96182ac224",
+    "url": "/Github-Finder/static/js/runtime-main.ad236092.js"
+  }
+]);
